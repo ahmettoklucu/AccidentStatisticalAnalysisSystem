b12f27f9db834745621a046ea9764b5b5ba73e8b
ahmettoklucu <toklucuahmet900@gmail.com>
2023-11-02 12:14:12 +0300

    STARTUP DOSYASI EKLENDİ.

---
+++ AccidentStatisticalAnalysisSystem.WepApi1/Startup.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+
+namespace AccidentStatisticalAnalysisSystem.WepApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Gerekli servislerin ve bağımlılıkların eklenmesi burada yapılır.
+            services.AddControllers(); // Web API kontrolcülerini etkinleştirir.
+            services.AddSwaggerGen(); // Swagger belgeleri eklemek için
+            // services.AddAuthentication(); // Kimlik doğrulama eklemek için
+            // services.AddAuthorization(); // Yetkilendirme eklemek için
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Your API V1"));
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
